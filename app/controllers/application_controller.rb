4346d94f70d7eae61cfe6376745ff8e4d9304754
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-05-23 18:17:09 +0200

    add pages/routes/tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  def welcome
-    render html: "Let's get started"
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#welcome'
+  get 'static_pages/about'
+  get 'static_pages/contacts'
+  root 'static_pages#home'
+
 end
