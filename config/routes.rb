c86efa5fd31d89d8a847be94dd8cded98de9180a
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-05-25 22:52:12 +0200

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contacts', to: 'static_pages#contacts'
+  get '/signup', to: 'users#new'
 end
