e431731a18cb648337038db53d194ab2a35d78ef
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-05-25 22:29:06 +0200

    add bootstrap,basic layout to homepage,add link,routes,and integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contacts'
-  
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contacts', to: 'static_pages#contacts'
 end
