99ec9e118cc3714fe6133f911a94b74eb5a277ff
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-05-26 18:47:31 +0200

    add User model,database migration user validity tests,presence,format

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 201},
+            format: { with: VALID_EMAIL_REGEX}
+            
+end
