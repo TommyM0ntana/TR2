daa90cf9ca5ddcae6b971170564faba316d03b2e
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-06-11 11:18:48 +0200

    update and complete the branch ready to merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 201},
             format: { with: VALID_EMAIL_REGEX},
             uniqueness: { case_sensitive: false }
-  has_secure_password 
+  has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
 def User.digest(string)
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,32 +5,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:tommy)
   end
-
-
-
-
-  
 #the main purpose is to be sure that clicking the submit button
 #is not creating new user
- test 'invalid signup information' do
-  get signup_path
-  assert_no_difference 'User.count' do
-  post users_path, params: { user: { name: '', email: 'user@invalid',
-                             password:'don', password_confirmation:'joe'}}
-    end
-    assert_template 'users/new'
-  end
+test "login with valid email/invalid password" do
+  get login_path
+  assert_template 'sessions/new'
+  post login_path, params: { session: { email:    @user.email,
+                                        password: "invalid" } }
+  assert_not is_logged_in?
+  assert_template 'sessions/new'
+  assert_not flash.empty?
+  get root_path
+  assert flash.empty?
+end
 
 
   #test to see if all the links are changing when user logs in
-  test 'link changes on valid submission and login' do
+test 'link changes on log in and log out' do
   get login_path
-  post login_path, params:  { session:
-                               { email: @user.email,
-                                 password: 'password'}}
+  post login_path, params: { session:
+                           { email: @user.email,
+                             password: 'password'}}
   assert is_logged_in?
   assert_redirected_to @user
-  !follow_redirect!
+  follow_redirect!
   assert_template 'users/show'
   assert_select 'a[href=?]', login_path, count:0
   assert_select 'a[href=?]', logout_path, count:1
