077371c23de067ad68365e64395f1a1c60fba362
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-06-26 16:43:01 +0200

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,12 +41,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'morning-retreat-44273.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
   # Mount Action Cable outside main process or domain.
